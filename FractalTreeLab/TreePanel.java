063ed5a5b57c5ed370e438548d44cfc5af8d0b37
Yudai Okabe <YOkabe@student.naperville203.org>
2016-03-17 13:43:22 -0500

    fasdfdsfe

--- FractalTreeLab/TreePanel.java
+++ FractalTreeLab/TreePanel.java
@@ -10,10 +10,10 @@ public class TreePanel extends JPanel
     private double lAngle1 = -5;
     private double rAngle1 = 20;
     
-    private double lAngle2 = 5;
-    private double rAngle2 = -20;
+    private double lAngle2 = 20;
+    private double rAngle2 = -5;
     
-    private double angle = 15;
+    
     
     public TreePanel()
     {
@@ -64,13 +64,41 @@ public class TreePanel extends JPanel
             g2.setColor(Color.cyan);
         }
         
-        double x2 = x + (Math.sin(Math.toRadians(currentAngle))*len);
-        double y2 = y + (Math.cos(Math.toRadians(currentAngle))*len);
+        double x2 = x - (Math.sin(Math.toRadians(currentAngle))*len);
+        double y2 = y - (Math.cos(Math.toRadians(currentAngle))*len);
         Line2D.Double line = new Line2D.Double(x, y, x2, y2);
         g2.draw(line);
         
-        drawFractal(g2, x2, y2, len - 10, currentAngle + lAngle1);
-        drawFractal(g2, x2, y2, len - 10, currentAngle - rAngle1);
+        drawFractal2(g2, x2, y2, len - 10, currentAngle + lAngle2);
+        drawFractal2(g2, x2, y2, len - 10, currentAngle - rAngle2);
+        
+        
+        
+    }
+    
+    public void drawFractal3(Graphics2D g2, double x, double y, double len, double currentAngle)
+    {
+        
+        if (len < 10)
+        {
+            return;
+        }
+        if (len < 80)
+        {
+            g2.setColor(Color.green);
+        }
+        else
+        {
+            g2.setColor(Color.cyan);
+        }
+        
+        double x2 = x - (Math.sin(Math.toRadians(currentAngle))*len);
+        double y2 = y - (Math.cos(Math.toRadians(currentAngle))*len);
+        Line2D.Double line = new Line2D.Double(x, y, x2, y2);
+        g2.draw(line);
+        
+        drawFractal3(g2, x2, y2, len - 10, currentAngle + 15);
+        drawFractal3(g2, x2, y2, len - 10, currentAngle - 15);
         
         
         
@@ -81,9 +109,9 @@ public class TreePanel extends JPanel
         Graphics2D g2 = (Graphics2D) g;
         super.paintComponent(g2);
         
-        
-        
+        drawFractal2(g2, 700, 900, 130, 0);
+        drawFractal3(g2, 700, 900, 130, 0);
         drawFractal(g2, 700, 900, 130, 0);
-        drawFractal(g2, 700, 900, 130, 0);
+        
     }
 }
